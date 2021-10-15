4b61902755cbd37c189e4217fe64c7bc5aa3ac0e
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2021-10-15 16:38:01 +0900

    useEffect の対象変数を追加

--- next_app/pages/index.tsx
+++ next_app/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import firebase from "firebase/app";
@@ -78,7 +79,7 @@ export default function Home(): JSX.Element {
           setData(mydata);
         });
     }
-  }, [user, show]);
+  }, [user, show, mydata]);
 
   const currentYMD = (e: number): string => {
     const yyyy = String(year);
